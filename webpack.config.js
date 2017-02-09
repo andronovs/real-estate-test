65c610c5f80da2c8175725e5cf027fdd82c07ff6
Sergiy Andronov <andronovs@hotmail.com>
2017-02-08 23:37:29 -0700

    Adding source maps and removing unused JS references

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ var config = {
       { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file' } 
     ]
   },
+  devtool: "source-map", 
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
